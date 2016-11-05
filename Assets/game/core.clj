dba4b8c09858c4d936be30f417ea4ffe58730025
selfsame <josphe.parker.1@gmail.com>
2016-11-05 19:11:22 -0400

    wasd movement for board

--- Assets/game/core.clj
+++ Assets/game/core.clj
@@ -3,9 +3,12 @@
     arcadia.core
     arcadia.linear
     tween.core
-    hard.core)
+    hard.core
+    hard.physics
+    hard.input)
   (import [SimpleTiledWFC]))
 
+(def player (atom nil))
 
 (defn make-park [w h]
   (let [o (clone! :maps/autopark)
@@ -18,14 +21,28 @@
 
 (defn make-player [loc]
   (let [loc (or loc (v3 0 10 0))
-        board (clone! :simple loc)]
+        board (clone! :board loc)]
     board))
 
+(defn handle-input [o]
+  (let [body (->rigidbody o)]
+  (if (key? "w") (force! body  40  0 0))
+  (if (key? "s") (force! body -40  0 0))
+  (if (key? "a") (torque! body 0 -20 0))
+  (if (key? "d") (torque! body 0  20 0))
+  (Input/GetAxis "Vertical")))
 
 (defn make-level []
   (clear-cloned!)
   (make-park 10 10)
-  (make-player (v3 5 5 5)))
+  (reset! player (make-player (v3 5 5 5)))
+  (hook+ @player :update #'game.core/handle-input)
+  )
 
-(make-level)
 
+
+'(timeline* :loop
+  (wait 1.0)
+  #(do (make-level) false))
+
+'(make-level) 
