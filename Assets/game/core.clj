e024a63244a5a5393c3e8fa8f32074688a708402
selfsame <josphe.parker.1@gmail.com>
2016-11-06 12:19:25 -0500

    skateboard physics stuff

--- Assets/game/core.clj
+++ Assets/game/core.clj
@@ -29,11 +29,16 @@
 
 (defn gizmo-cam [o]
   (gizmo-color (color 1 0 0))
-  (gizmo-line (>v3 o) (>v3 @player)))
+  (gizmo-line (>v3 o) (>v3 @player))
+  (gizmo-color (color 0 1 1))
+  (gizmo-line  (>v3 @player) (.TransformPoint (.transform @player) (v3 0 -1.0 0))))
 
 (defn upsidedown? [o]
   (and (hit (>v3 o) (.TransformDirection (.transform o) (v3 0 1 0)))
-       (not (hit (>v3 o) (.TransformDirection (.transform o) (v3 0 -1 0))))))
+       #_(not (hit (>v3 o) (.TransformDirection (.transform o) (v3 0 -1 0)))) ))
+
+(defn wheel-contact? [o]
+  (and (first (range-hits (>v3 o) (.TransformDirection (.transform o) (v3 0 -1 0)) 1.0))))
 
 (defn ->wheel [o] (cmpt o UnityEngine.WheelCollider))
 
@@ -42,6 +47,11 @@
            (->wheel (child-named o "front-left"))]
    :rear  [(->wheel (child-named o "rear-right"))
            (->wheel (child-named o "rear-left"))]})
+
+(defn steer [n col]
+  (dorun (map #(set! (.steerAngle %) (float n)) col)))
+(defn motor [n col]
+  (dorun (map #(set! (.motorTorque %) (float n)) col)))
 
 (defn make-player [loc]
   (let [loc (or loc (v3 0 10 0))
@@ -54,32 +64,62 @@
 (defn handle-input [o]
   (let [body (->rigidbody o)
         mass (.mass (->rigidbody o))
-        wheels (wheelmap o)]
-  (if (key? "w") (force!  body 0 0 (* mass  20)))
-  (if (key? "s") (force!  body 0 0 (* mass -20)))
+        wheels (wheelmap o)
+        motorforce (* mass 1)]
+  (cond 
+    (key? "w") 
+    (do (force! body 0 0 (* mass  10))
+        (motor motorforce (:rear wheels))
+        (motor motorforce (:front wheels)))
+    (key? "s") 
+    (do (force! body 0 0 (* mass  -10))
+        (motor (- motorforce) (:rear wheels))
+        (motor (- motorforce) (:front wheels)))
+    
+    :else
+    (do (motor 0 (:rear wheels))
+        (motor 0 (:front wheels))))
   (cond 
     (key? "a") 
-    (mapv #(set! (.steerAngle %) (float 20)) (flatten ((juxt :rear) wheels)))
+    (do (steer 20 (:rear wheels))
+        (steer -20 (:front wheels)))
     (key? "d") 
-    (mapv #(set! (.steerAngle %) (float -20)) (flatten ((juxt :rear) wheels)))
+    (do (steer -20 (:rear wheels))
+        (steer 20 (:front wheels)))
     :else
-    (mapv #(set! (.steerAngle %) (float 0)) (flatten ((juxt :rear) wheels))))
+    (do (steer 0 (:rear wheels))
+        (steer 0 (:front wheels))))
   ;(if (key? "d") (torque! body 0 (* mass  12) 0))
   (if (and (key? "space") (upsidedown? o)) 
-    (torque! body 0 0 (* mass  20)))
+    (torque! body 0 0 (* mass  60)))
+  (if (wheel-contact? o) (force! body 0 (* mass -6) 0))
   (Input/GetAxis "Vertical")))
 
 (defn make-level []
   (clear-cloned!)
-  (make-park 14 14)
+  (make-park 16 16)
   (reset! player (make-player (v3 10 10 10)))
   (hook+ @player :update #'game.core/handle-input))
 
 
 
+
+
+
+(defn message [s]
+  (destroy (the message))
+  (let [cam (the skatecam)
+        o (clone! :message (.TransformPoint (.transform cam) (v3 0 0 10)))
+        txt (cmpt (child-named o "text") UnityEngine.TextMesh)]
+    (parent! o cam)
+    (set! (.text txt) s)
+    (timeline [#(lerp-look! o cam (float 0.2))])))
+
+
 '(timeline* :loop
   (wait 3.0)
-  #(do (make-level) false))
+  #(do (make-level) nil)
+  (wait 0.1)
+  #(do (message "snack time") nil))
 
 '(make-level)
-
---
+++ Assets/game/todo.clj
@@ -0,0 +1,9 @@
+(ns game.todo)
+
+::skateboard
+'(( ) fn for checking if wheels are on ground)
+'(( ) apply downward force to board when not in air)
+'(( ) when in air allow twisting torque)
+'(( ) analog steering)
+'(( ) get sideways friction right
+  (( ) when board quickly twists 180 reverse front direction))
